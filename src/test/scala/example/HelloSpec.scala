be1979b8787a824a96782e88867c7538d8862235
vamsiampolu <vamsiampolu3000@gmail.com>
2017-12-01 13:58:56 +1100

    Update with 6/33 of scala-exercises

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -21,6 +21,25 @@ object PointService {
   }
 }
 
+class Movie(val name: String, val year: Short)
+
+object Movie {
+  def academyAwardBestMovieForYear(x: Short) = {
+    x match {
+      case 1930 ⇒ Some(new Movie("All Quiet On the Western Front", 1930))
+      case 1931 ⇒ Some(new Movie("Cimarron", 1931))
+      case 1932 ⇒ Some(new Movie("Grand Hotel", 1932))
+      case _    => None
+    }
+  }
+}
+
+class Person(val name: String, private val alterEgo: String)
+
+object Person {
+  def secretRevealed: Person => String = x => x.alterEgo
+}
+
 class HelloSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "hello"
@@ -139,4 +158,133 @@ class HelloSpec extends FlatSpec with Matchers {
     c eq a should be(true)
     OptionExample eq c should be(true)
   }
+
+  "Companion Object" should "contain factory methods" in {
+    Movie.academyAwardBestMovieForYear(1932).get.name should be("Grand Hotel")
+  }
+
+  // This does not work on the REPL ?? 
+  "Companion Object" should "be able to access private members" in {
+    val clark = new Person("Clark Kent", "Superman")
+    val result = Person.secretRevealed(clark)
+    result should be("Superman")
+  }
+
+  "Tuple members" should "be accesed using _1 and so on" in {
+    val t = ("Apples", "Dog")
+    val fruits = t._1
+    val animal = t._2
+    fruits should be("Apples")
+    animal should be("Dog")
+  }
+
+  "Tuple members" should "be accessed using destructuring" in {
+    val student = ("Sean Rogers", 21, 3.5)
+    val (name, age, gpa) = student
+    name should be("Sean Rogers")
+    age should be(21)
+    gpa should be(3.5)
+  }
+
+  "Tuple of size 2" should "allow members to be swapped" in {
+    val original = ("apple", 3)
+    val (fruit, qty) = original
+    val result = original.swap
+    result._1 shouldBe qty
+    result._2 shouldBe fruit
+  }
+
+  "Lambda" should "work" in {
+    def fn = (x:Int) => x + 1
+    val fn_no_sugar = new Function1[Int, Int] {
+      def apply(a: Int) = {
+        a + 1
+      }
+    }
+    val result = fn(1)
+    result shouldBe  2
+  }
+
+  "Lambda" should "use closures" in {
+    val y = 2
+    def fn = (x: Int) => {
+      x + y
+    }
+    val result = fn(4)
+    result shouldBe 6
+  }
+
+  "Lambda" should  "accept functions" in {
+    val x = 2
+    val fn = (y: Int) => { x + y + 1}
+    val fn2 = (a: Int, closure:Int => Int) => {
+      closure(a)
+    }
+    val result = fn2(3, fn)
+    result shouldEqual 6
+  }
+
+  // without syntax sugar
+  "Lambda" should "return functions" in {
+    val x = 2
+    val fn = (y: Int) => {
+      // return value has been desugared
+      new Function1[Int, Int] {
+        def apply(z:Int) = {
+          x + y + z
+        }
+      }
+    }
+    val fn2 = fn(3)
+    val result = fn2(1)
+    result should be(6)
+  }
+
+  // with syntax sugar
+  "Lambda" should "return functions2" in {
+      val x = 2
+      val fn = (y:Int) => { (z:Int) => x + y + z }
+      val fnb = fn(3)
+      val result = fnb(1)
+      result should be(6)
+  }
+
+  "Lambda" should "work with isInstanceOf" in {
+    val fn = (y:Int) => y + 1
+    fn.isInstanceOf[Function1[Int, Int]] should be(true)
+  }
+
+  "Lambda" should "work with shouldBe a " in {
+    val fn = (y:Int) => y + 1
+    fn shouldBe a[Function1[_, _]]
+  }
+
+  "Lambda" should "be passed to map" in {
+    def makeUpper: List[String] => List[String] = xs => xs map {
+      _.toUpperCase
+    }
+
+    val a = List("abc", "xyz", "123")
+    val b = makeUpper(a)
+    val expectedB = List("ABC", "XYZ", "123")
+
+    b should be(expectedB)
+
+    def makeWhateverYouLike(xs: List[String], sideEffect: String => String) = {
+      xs map sideEffect
+    }
+
+    val c = makeWhateverYouLike(b, { x => x.toLowerCase })
+    c should be(a)
+
+    val d = List("John", "Mark")
+    def myName(name:String) = s"My name is $name"
+    val expectedE = List("My name is John", "My name is Mark")
+    val e = makeWhateverYouLike(d, myName)
+
+    List("Scala", "Erlang", "Clojure") map (_.length) should be(
+      List(5,6,7)
+)
+  }
 }
+
